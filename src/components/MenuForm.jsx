8a1339f1ce24bd5d70e7073bfb57cc2beea16e2a
unknown <kayv@hotmail.fr>
2024-06-09 16:34:22 +0200

    update
    -Add burger
    - Add sides

--- src/components/MenuForm.jsx
+++ src/components/MenuForm.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import '../styles/menuForm.css'
+
 export default function MenuForm({menuTitle, menuDescription, menuImage, menuRecipe, menuPrice, addArticle}){
-   
     return (
 			<article className="menuForm">
 				<img className="menuForm__image" src={menuImage} alt={menuTitle} />
 				<div className="menuForm__info">
 					<h2 className="menuForm__info__title">{menuTitle}</h2>
+					<p className="menuForm__info__description">{menuDescription}</p>
+					<ul className="menuForm__info__list">
+						{menuRecipe.map((item, key) => (
+							<li className="menuForm__info__list__item">
+								{item}
+								{', '}
+							</li>
+						))}
+					</ul>
 					<span className="menuForm__info__price">{menuPrice}€</span>
 				</div>
-				<button className="menuForm__btn" onClick={addArticle}>COMMANDER</button>
+				<button className="menuForm__btn" onClick={addArticle}>
+					COMMANDER
+				</button>
 			</article>
 		);
 }
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -10,28 +10,25 @@ import Modal from '../components/Modal';
 import '../styles/menu.css';
 
 export default function Menu() {
-	const dispatch = useDispatch()
-	const newOrder = useSelector((state) => state.order)
+	const dispatch = useDispatch();
+	const newOrder = useSelector((state) => state.order);
 
 	// order variables
 	const [cart, setCart] = useState([]);
 	const [totalPrice, setTotalPrice] = useState(0);
-	const [orderId, setOrderId] = useState(0)
+	const [orderId, setOrderId] = useState(0);
 	const [isCartReset, setIsCartReset] = useState(false);
 	//modal variables
-	const [isOpenModal, setIsModalOpen] = useState(true)
-
-
+	const [isOpenModal, setIsModalOpen] = useState(true);
 
 	useEffect(() => {
 		console.log(newOrder);
 	}, [newOrder]);
 
-	// ORDER FUNCTIONS 
+	// ORDER FUNCTIONS
 	function addToCart(menuTitle, burgerPrice) {
 		setCart([...cart, { title: menuTitle, price: parseFloat(burgerPrice) }]);
 		setTotalPrice(parseFloat(totalPrice) + parseFloat(burgerPrice));
-		
 	}
 
 	//remove articles from the order
@@ -44,19 +41,19 @@ export default function Menu() {
 	}
 
 	//dispatch the order
-	function handleOrder(){
-		setOrderId(ticketNumber(1,100))
+	function handleOrder() {
+		setOrderId(ticketNumber(1, 100));
 		dispatch(addOrder(cart, totalPrice, orderId));
 		//setIsCartReset(true);
 		setIsModalOpen(true);
 	}
 	//generate a random number for the ticket Id
-	function ticketNumber(min, max){
+	function ticketNumber(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 
-	function closeModal(){
-		setIsModalOpen(false)
+	function closeModal() {
+		setIsModalOpen(false);
 		//setIsCartReset(false)
 	}
 
@@ -70,28 +67,42 @@ export default function Menu() {
 						key={burger.id}
 						menuTitle={burger.title}
 						menuImage={burger.image}
+						menuDescription={burger.description}
+						menuRecipe={burger.burger.ingredients}
 						menuPrice={burger.price}
 						addArticle={() => addToCart(burger.title, burger.price)}
 					/>
 				))}
 			</div>
+			<h1 className="menu__title title">Nos Burgers</h1>
+			<div className="menu">
+				{burgers.map((burger, key) => (
+					<MenuForm
+						key={burger.id}
+						menuTitle={burger.burger.name}
+						menuImage={burger.burger.image}
+						menuRecipe={burger.burger.ingredients}
+						menuPrice={burger.burger.price}
+						addArticle={() =>
+							addToCart(burger.burger.name, burger.burger.price)
+						}
+					/>
+				))}
+			</div>
 
-			{isCartReset ? 
+			{isCartReset ? (
 				<Cart cart={[]} />
-			 : 
+			) : (
 				<Cart
 					cart={cart}
 					removeArticle={removeArticle}
 					totalPrice={totalPrice}
 					handleOrder={handleOrder}
 				/>
-			}
-			{isOpenModal ??
-				<Modal 
-					message="Merci d'avoir commandé !"
-					closeModal={closeModal}
-				/>
-			}
+			)}
+			{isOpenModal ?? (
+				<Modal message="Merci d'avoir commandé !" closeModal={closeModal} />
+			)}
 		</>
 	);
 }
